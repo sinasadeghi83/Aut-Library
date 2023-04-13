12395ba95d7dc7a048f3c15bd9af7040bedd831a
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-04-13 20:59:03 +0330

    Add editThesis to ThesisController

--- src/com/sinatech/controllers/ThesisController.java
+++ src/com/sinatech/controllers/ThesisController.java
@@ -1,10 +1,7 @@
 package com.sinatech.controllers;
 
 import com.sinatech.components.DatabaseManager;
-import com.sinatech.models.Category;
-import com.sinatech.models.Library;
-import com.sinatech.models.Response;
-import com.sinatech.models.Thesis;
+import com.sinatech.models.*;
 
 public class ThesisController {
     public static Response addThesis(Thesis thesis) {
@@ -20,6 +17,31 @@ public class ThesisController {
             return new Response(1); //Returns duplicate-id
         }
         library.addThesis(thesis);
+        return new Response(0); //Returns success
+    }
+
+    public static Response editThesis(Thesis editThesis) {
+        //Check if library exist in database
+        Library library = DatabaseManager.getLibrary(editThesis.getLibId());
+        if(library == null){
+            return new Response(2); //Returns not-found
+        }
+
+        //Check if category exist
+        if(editThesis.getCatId() != null) {
+            Category category = DatabaseManager.getCategory(editThesis.getCatId());
+            if (category == null) {
+                return new Response(2); //Returns not-found
+            }
+        }
+
+        Thesis thesis = library.getThesis(editThesis.getId());
+        //Check if book exist in the library
+        if(thesis == null){
+            return new Response(2); //Returns not-found
+        }
+
+        thesis.update(editThesis);
         return new Response(0); //Returns success
     }
 }
