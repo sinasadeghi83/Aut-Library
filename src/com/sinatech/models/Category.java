002ecdf492b739a0e8e14c0ed5611d44f39cd6c0
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-04-13 16:58:56 +0330

    Add paperIds fields and its add method to Category

--- src/com/sinatech/models/Category.java
+++ src/com/sinatech/models/Category.java
@@ -1,12 +1,18 @@
 package com.sinatech.models;
 
+import java.util.HashSet;
+
 public class Category {
     private String id;
     private String name;
 
+    //A collection of Book and thesis ids which belong to this category
+    private HashSet<String> paperIds;
+
     public Category(String id, String name) {
         this.id = id;
         this.name = name;
+        this.paperIds = new HashSet<>();
     }
 
     public String getId() {
@@ -15,5 +21,9 @@ public class Category {
 
     public String getName() {
         return name;
+    }
+
+    public void addPaperId(String id){
+        paperIds.add(id);
     }
 }
