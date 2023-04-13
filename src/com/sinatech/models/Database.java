f07f7bfa43d721ebcb9a0d61a63deb9ae1021228
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-04-13 15:22:29 +0330

    Add Database class and libraries in it to models

---
+++ src/com/sinatech/models/Database.java
@@ -0,0 +1,13 @@
+package com.sinatech.models;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Database {
+    HashMap<String, Library> libraries;
+
+    public Database() {
+        this.libraries = new HashMap<>();
+    }
+}
