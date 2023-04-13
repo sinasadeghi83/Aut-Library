42be8e102fb989639297db9fe2ac03f264d6d85f
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-04-13 15:31:14 +0330

    Add getDefaultMessage and constructor by id for Response class

--- src/com/sinatech/models/Response.java
+++ src/com/sinatech/models/Response.java
@@ -2,12 +2,17 @@ package com.sinatech.models;
 
 public class Response {
 
-    public static final String[] ID_MSG = new String[]{
+    public static final String[] ID_DEFAULT_MSG = new String[]{
             "success", //id: 0
             "duplicate-id" //id: 1
     };
     private int id;
     private String message;
+
+    public Response(int id) {
+        this.id = id;
+        this.message = getDefaultMessage(id);
+    }
 
     public Response(int id, String message) {
         this.id = id;
@@ -20,5 +25,13 @@ public class Response {
 
     public String getMessage() {
         return message;
+    }
+
+    public String getDefaultMessage(int id){
+        if(ID_DEFAULT_MSG.length <= id){
+            System.err.println("Index out of range");
+            return "Err!";
+        }
+        return ID_DEFAULT_MSG[id];
     }
 }
