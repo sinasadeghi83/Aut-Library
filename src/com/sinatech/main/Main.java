64edc2769afa8a64058bafe91e0c3dcb4c52d972
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-04-13 16:50:32 +0330

    Add Command reader and runner to Main

--- src/com/sinatech/main/Main.java
+++ src/com/sinatech/main/Main.java
@@ -1,7 +1,19 @@
 package com.sinatech.main;
 
-public class Main {
-    public static void main(String[] args) {
+import com.sinatech.models.Command;
+import com.sinatech.models.Database;
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Scanner scanner = new Scanner(System.in);
+        String cmdStr = scanner.nextLine();
+        Database myDb = new Database();
+        while (!cmdStr.equals("finish")){
+            Command command = new Command(cmdStr, myDb);
+            command.runCommand();
+            cmdStr = scanner.nextLine();
+        }
     }
 }
