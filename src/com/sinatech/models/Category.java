af186fb2da4ea2321772a81cfa6f91f79406f3c2
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-04-13 16:52:59 +0330

    Add Category class to models

---
+++ src/com/sinatech/models/Category.java
@@ -0,0 +1,19 @@
+package com.sinatech.models;
+
+public class Category {
+    private String id;
+    private String name;
+
+    public Category(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
