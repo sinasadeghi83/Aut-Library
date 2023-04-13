61be181a5fd65261abfc5a2808eda28e2aa03499
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-04-14 00:10:55 +0330

    Add StudentController and addStudent method to it

--- src/com/sinatech/controllers/StudentController.java
+++ src/com/sinatech/controllers/StudentController.java
@@ -1,2 +1,16 @@
-package com.sinatech.controllers;public class StudentController {
+package com.sinatech.controllers;
+
+import com.sinatech.components.DatabaseManager;
+import com.sinatech.models.Response;
+import com.sinatech.models.Student;
+
+public class StudentController {
+
+    public static Response addStudent(Student student) {
+        if(DatabaseManager.getStudent(student.getId()) != null){
+            return new Response(1); //returns duplicate-id
+        }
+        DatabaseManager.insertStudent(student);
+        return new Response(0); //returns success
+    }
 }
