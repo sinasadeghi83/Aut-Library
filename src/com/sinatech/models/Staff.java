7260003aaf3d814556f0590fb872632273d89855
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-04-14 00:35:16 +0330

    Add Staff to models

---
+++ src/com/sinatech/models/Staff.java
@@ -0,0 +1,71 @@
+package com.sinatech.models;
+
+import java.util.Date;
+
+public class Staff {
+    private String id, password, firstName, lastName, nationalCode;
+    private Date birthdate;
+    private String address;
+
+    public Staff(String id, String password, String firstName, String lastName, String nationalCode, Date birthdate, String address) {
+        this.id = id;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nationalCode = nationalCode;
+        this.birthdate = birthdate;
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
