9c2d6dceef844d8635de80fc2da177bc341e61ac
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-04-14 15:31:00 +0330

    Add invalid-pass & not-allowed to Response

--- src/com/sinatech/models/Response.java
+++ src/com/sinatech/models/Response.java
@@ -5,7 +5,9 @@ public class Response {
     public static final String[] ID_DEFAULT_MSG = new String[]{
             "success", //id: 0
             "duplicate-id", //id: 1
-            "not-found" //id: 2
+            "not-found", //id: 2
+            "not-allowed", //id: 3
+            "invalid-pass", //id: 4
     };
     private int id;
     private String message;
