ef9c590c87ab4565332c4990b635393db0c5c31e
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-04-14 15:30:02 +0330

    Add borrows field and its methods to Library

--- src/com/sinatech/models/Library.java
+++ src/com/sinatech/models/Library.java
@@ -1,5 +1,8 @@
 package com.sinatech.models;
 
+import com.sinatech.components.DatabaseManager;
+
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 
@@ -13,6 +16,8 @@ public class Library {
     private HashMap<String, Book> books;
     //Thesis' id -> Thesis' object
     private HashMap<String, Thesis> theses;
+    //Book id -> Borrow object
+    private HashMap<String, ArrayList<Borrow>> borrows;
 
     public Library(String id, String name, Date foundDate, int tableCount, String address) {
         this.id = id;
@@ -22,6 +27,7 @@ public class Library {
         this.address = address;
         this.books = new HashMap<>();
         this.theses = new HashMap<>();
+        this.borrows = new HashMap<>();
     }
 
     public String getId() {
@@ -67,5 +73,26 @@ public class Library {
 
     public void removeThesis(String id) {
         this.theses.remove(id);
+    }
+
+    public boolean borrowBook(Borrow borrow){
+        Book book = this.getBook(borrow.getPaperId());
+        ArrayList<Borrow> borrowList = borrows.computeIfAbsent(borrow.getPaperId(), k -> new ArrayList<>());
+        if(borrowList.size() == book.getCopyCount()){
+            return false;
+        }
+        borrowList.add(borrow);
+        borrows.put(borrow.getPaperId(), borrowList);
+        return true;
+    }
+
+    public boolean borrowThesis(Borrow borrow) {
+        ArrayList<Borrow> borrowList = borrows.computeIfAbsent(borrow.getPaperId(), k -> new ArrayList<>());
+        if(borrowList.size() == 1){
+            return false;
+        }
+        borrowList.add(borrow);
+        borrows.put(borrow.getPaperId(), borrowList);
+        return true;
     }
 }
